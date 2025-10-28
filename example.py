15a23a93cc030e2337a303db766a10fa3e88a3fd
Nathan Gu <198284814+nayugu@users.noreply.github.com>
2025-10-28 11:29:47 -0400

    feat: Add example.py

---
+++ example.py
@@ -0,0 +1,20 @@
+from my_torch import *
+
+a = Tensor([2],requires_grad=True)
+b = Tensor([3],requires_grad=True)
+c = Tensor([4],requires_grad=True)
+d = Tensor([5],requires_grad=True)
+
+x = a * b * c * d
+y = 2 * a + b + c + d
+
+x_leaves = x.backward()
+y_leaves = y.backward()
+
+print_partial_d(x_leaves)
+print_partial_d(y_leaves)
+
+print(a.grad)
+print(b.grad)
+print(c.grad)
+print(d.grad)
