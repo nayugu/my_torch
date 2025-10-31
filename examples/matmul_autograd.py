a9438a09f4939274510df4078668b3ac5b2b341d
Nathan Gu <198284814+nayugu@users.noreply.github.com>
2025-10-31 17:16:45 -0400

    chore: Create new example file showcasing matrix multiplication and autograd

---
+++ examples/matmul_autograd.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append("../my_torch")
+import my_torch as torch
+
+w = torch.Tensor([[-1,1]], name='w', requires_grad=True)
+x = torch.Tensor([[2,3],
+                  [4,5]], name='x', requires_grad=True)
+b = torch.Tensor([[-2,-2]], name='b', requires_grad=True)
+
+z = (w @ x) + b ; z.name = 'z'
+a = z.sigmoid() ; a.name = 'a'
+
+print(z)
+print(a)
+a_leaves = a.backward()
+
+torch.print_partial_d(a_leaves)
