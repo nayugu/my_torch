afc681d6ea13dc10a0247c8bbeb5e50afcca2221
Nathan Gu <198284814+nayugu@users.noreply.github.com>
2025-11-05 13:57:41 -0500

    feat: Create zero_grad and SGD
    fix: Make model_params stored as a list because generators can only be used once

--- my_torch/optim.py
+++ my_torch/optim.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 # region Optimizers
 class Optimizer(ABC): # Define common optimizer interface
     def __init__(self,model_params,lr):
-        self.model_params = model_params
+        self.model_params = list(model_params)
         self.lr = lr
 
     @ abstractmethod
@@ -13,10 +13,24 @@ class Optimizer(ABC): # Define common optimizer interface
         # Update .data in place with gradients
         pass
 
+    def zero_grad(self) -> None:
+        for p in self.model_params:
+            p.grad = None
+
+    def __str__(self):
+        output = ""
+        for p in self.model_params:
+            output += f"{p.name}\n"
+        return output
+
+
 class SGD(Optimizer):
     # Vanilla/base optimizer
     def step(self) -> None:
-        pass
+        for param in self.model_params:
+            if param.grad is None:
+                continue
+            param.data -= self.lr * param.grad
 
 class Adam(Optimizer):
     # Adaptive Momemtum Estimation
