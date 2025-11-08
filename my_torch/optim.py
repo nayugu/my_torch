627c666b1518dbd8b200eeca3b08f2f7c1d3b08f
Nathan Gu <198284814+nayugu@users.noreply.github.com>
2025-11-07 19:54:52 -0500

    fix: Add time step to Adam optimizer

--- my_torch/optim.py
+++ my_torch/optim.py
@@ -38,20 +38,22 @@ class Adam(Optimizer):
         super().__init__(model_params, lr)
         self.b1 = b1 # for Momentum
         self.b2 = b2 # for Squared gradients
+        self.t = 0 # time step
 
         self.v = {param:0 for param in self.model_params}
         self.s = {param:0 for param in self.model_params}
 
     def step(self) -> None:
+        self.t += 1
         for param in self.model_params:
             if param.grad is None:
                 continue
             self.v[param] = self.b1 * self.v[param] + (1-self.b1) * param.grad
             self.s[param] = self.b2 * self.s[param] + (1-self.b2) * (param.grad ** 2)
 
-            # Correct bias
-            _v = self.v[param] / (1-self.b1)
-            _s = self.s[param] / (1-self.b2)
+            # Correct bias for initial time steps
+            _v = self.v[param] / (1-self.b1**self.t)
+            _s = self.s[param] / (1-self.b2**self.t)
             weighted_grad = _v/((_s**0.5)+1e-16)
             
             param.data -= self.lr * weighted_grad
