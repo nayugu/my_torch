fix: Account for broadcasting of scalars into tensors and check if both new_ accumulated_grad and d_sub_node are tensors before attempting to unbroadcast
feat: Update print process of backprop with better formatting, tabulating depth, shape, and tensor names

my_torch/tensor.py
@@ -52,8 +52,8 @@ class Tensor:
             leaves: dict[Tensor,np.ndarray] = {}
         
         if print_process:
-            print("" if depth==0 \
-                  else f"{depth:<3}" + " | "*(depth-1), node.name, node.shape)
+            print("depth  shape.......  tensor\n-----  ------------  ------" if depth==0 \
+                  else f"{depth:<5}  {str(node.shape):<12}  {" | "*(depth-1)}{node.name}")
 
         # If the current node is a leaf, update it
         if node.partial_d == {}:
@@ -95,7 +95,18 @@ class Tensor:
                     new_accumulated_grad = accumulated_grad * d_sub_node
 
                     # Check for broadcasting
-                    if new_accumulated_grad.shape != sub_node.shape:
+
+                    # Both input and output are scalars
+                    if isinstance(new_accumulated_grad,float) or isinstance(new_accumulated_grad,np.float64):
+                        pass
+                    
+                    # # Input is a broadcasted tensor but input is a scalar
+                    # elif isinstance(new_accumulated_grad,np.ndarray) and \
+                    #     (isinstance(d_sub_node,float) or isinstance(d_sub_node,np.float64)): 
+                    #     new_accumulated_grad = un_broadcast(new_accumulated_grad,tuple(np.oneslike(sub_node.shape)))
+
+                    # Both input and output are tensors
+                    elif new_accumulated_grad.shape != sub_node.shape:
                         new_accumulated_grad = un_broadcast(new_accumulated_grad,sub_node.shape)
 
                 sub_node.recursive_chain_rule(leaves=leaves,